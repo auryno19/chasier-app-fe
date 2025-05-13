df6b3e5738477cd65f5c91ff8f16730744958052
auryno19 <aurynoaga19@gmail.com>
2025-05-13 09:27:32 +0700

    first commit

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import Image from "next/image";
+import ListMenu from "./listMenu";
+import { useEffect, useState } from "react";
+import ListMenuDropDown from "./listMenuDropdown";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth >= 768) {
+      setIsOpen(true);
+      setIsVisible(true);
+    } else {
+      setIsOpen(false);
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div
+      className={`relative bg-sky-50 h-[96vh] rounded-lg shadow-lg py-4 transition-all duration-300 ${
+        isOpen ? "w-[16%] px-3" : "w-[40px] px-1"
+      }`}
+    >
+      <div
+        className={`w-9 h-9 rounded-full shadow-md bg-slate-500 absolute z-20 -right-4 top-1/2 -translate-y-1/2 flex items-center justify-center cursor-pointer ${
+          !isVisible && "hidden"
+        }`}
+        onClick={handleToggle}
+      >
+        <span
+          className={`eva--chevron-left-fill transition-all duration-500 ${
+            !isOpen && "scale-x-[-1]"
+          }`}
+        ></span>
+      </div>
+      <div className="relative w-full h-5 mt-2">
+        <Image
+          src={`${isOpen ? "/next.svg" : "/next-sm.svg"}`}
+          layout="fill"
+          objectFit="contain"
+          alt="Fufa Store Logo"
+          className="w-0"
+        />
+      </div>
+      <div className="flex w-full justify-center">
+        <hr className=" w-[80%] items-center my-3 border-slate-600 opacity-75" />
+      </div>
+      <ListMenu
+        menu="Dashboard"
+        isOpen={isOpen}
+        link="/dashboard"
+        padding=""
+        icon="mage--dashboard-3-fill"
+      />
+      <ListMenu
+        menu="Banner"
+        isOpen={isOpen}
+        link="/dashboard/banner"
+        padding=""
+        icon="icon-park-solid--tent-banner"
+      />
+      <ListMenuDropDown
+        menu="Dropdown"
+        isOpen={isOpen}
+        icon="tdesign--component-dropdown-filled"
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Sidebar from "./components/sidebar";
+import Navbar from "./components/navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "Dashboard | %s",
+    default: "Dashboard",
+  },
+  description: "Chasier Application Dashboard by Auryno",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="flex p-3 flex-row gap-2">
+          <Sidebar />
+          <div className="flex flex-col w-full gap-2">
+            <Navbar />
+            <div className="px-6 py-4 w-full h-[85.5vh] text-slate-600 bg-sky-50 rounded-lg overflow-y-auto">
+              {children}
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
